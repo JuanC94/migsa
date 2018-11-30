3ffc7938363fc3c894de008426bad6f89331b639
unknown <juanlarapicon@gmail.com>
2018-11-29 23:37:25 -0500

    Cambio url app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://migsa'),
+    'url' => env('APP_URL', 'https://damp-brushlands-45902.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
